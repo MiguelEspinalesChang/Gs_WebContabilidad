2d7906b1014aeebf100d27f138ccc06a24e48037
MiguelEspinalesChang <MiguelEspinalesChang_Programador@Github.com>
2017-08-26 11:17:32 -0600

    Agregar clase cliente y css

---
+++ GeneralStore_Web/GeneralStore_Web/Clases/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore_Web.Clases
+{
+    public class Cliente
+    {
+        private string Nombre;
+
+        public string Nombre1
+        {
+            get
+            {
+                return Nombre;
+            }
+
+            set
+            {
+                Nombre = value;
+            }
+        }
+    }
+}
