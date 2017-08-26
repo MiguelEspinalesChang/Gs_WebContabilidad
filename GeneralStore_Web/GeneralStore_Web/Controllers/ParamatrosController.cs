fd75b5c30246265a176fd64527623729271fb45e
MiguelEspinalesChang <MiguelEspinalesChang_Programador@Github.com>
2017-08-26 10:56:16 -0600

    parametros controler

---
+++ GeneralStore_Web/GeneralStore_Web/Controllers/ParamatrosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore_Web.Controllers
+{
+    public class ParamatrosController : Controller
+    {
+        // GET: Paramatros
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
