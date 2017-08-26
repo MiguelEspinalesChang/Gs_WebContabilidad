e3246b944d4b8359d3fbe6890ccb9aacfd779b97
MiguelEspinalesChang <MiguelEspinalesChang_Programador@Github.com>
2017-08-26 12:03:26 -0600

    agregar atributos en clientes

--- GeneralStore_Web/GeneralStore_Web/Clases/Cliente.cs
+++ GeneralStore_Web/GeneralStore_Web/Clases/Cliente.cs
@@ -7,18 +7,132 @@ namespace GeneralStore_Web.Clases
 {
     public class Cliente
     {
-        private string Nombre;
+        private string nombre;
+        private string apellido;
+        private short telefono ;
+        private string cedula;
+        private string correo;
+        private string empresa;
+        private string atencion;
+        private int numeroCliente;
+        private string condicionPago;
+             
 
-        public string Nombre1
+
+        public string Nombre
         {
             get
             {
-                return Nombre;
+                return nombre;
             }
 
             set
             {
-                Nombre = value;
+                nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+
+            set
+            {
+                apellido = value;
+            }
+        }
+
+        public short Telefono
+        {
+            get
+            {
+                return telefono;
+            }
+
+            set
+            {
+                telefono = value;
+            }
+        }
+
+        public string Cedula
+        {
+            get
+            {
+                return cedula;
+            }
+
+            set
+            {
+                cedula = value;
+            }
+        }
+
+        public string Correo
+        {
+            get
+            {
+                return correo;
+            }
+
+            set
+            {
+                correo = value;
+            }
+        }
+
+        public string Empresa
+        {
+            get
+            {
+                return empresa;
+            }
+
+            set
+            {
+                empresa = value;
+            }
+        }
+
+        public string Atencion
+        {
+            get
+            {
+                return atencion;
+            }
+
+            set
+            {
+                atencion = value;
+            }
+        }
+
+        public int NumeroCliente
+        {
+            get
+            {
+                return numeroCliente;
+            }
+
+            set
+            {
+                numeroCliente = value;
+            }
+        }
+
+        public string CondicionPago
+        {
+            get
+            {
+                return condicionPago;
+            }
+
+            set
+            {
+                condicionPago = value;
             }
         }
     }
