de3bba94ebedf6893eb3323e92c624903489b021
MiguelEspinalesChang <MiguelEspinalesChang_Programador@Github.com>
2017-09-02 17:22:22 -0600

    agrar clases bodega items

---
+++ GeneralStore_Web/GeneralStore_Web/Clases/Bodega.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore_Web.Clases
+{
+    public class Bodega
+    {
+
+        private int idBodega;
+        private string descripcion;
+        private int idItem;
+        
+
+
+        public int IdBodega
+        {
+            get
+            {
+                return idBodega;
+            }
+
+            set
+            {
+                idBodega = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                descripcion = value;
+            }
+        }
+
+        public int IdItem
+        {
+            get
+            {
+                return idItem;
+            }
+
+            set
+            {
+                idItem = value;
+            }
+        }
+    }
+}
---
+++ GeneralStore_Web/GeneralStore_Web/Clases/Item.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore_Web.Clases
+{
+    public class Item
+    {
+
+        private int idItem;
+        private string descripcion;
+        private string ubicacion;
+        private int cantidad;
+        private string marca;
+        private string codigo;
+        private int precioListaCordoba;
+        private int precioListaDollar;
+
+
+        public int IdItem
+        {
+            get
+            {
+                return idItem;
+            }
+
+            set
+            {
+                idItem = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                descripcion = value;
+            }
+        }
+
+        public string Ubicacion
+        {
+            get
+            {
+                return ubicacion;
+            }
+
+            set
+            {
+                ubicacion = value;
+            }
+        }
+
+        public int Cantidad
+        {
+            get
+            {
+                return cantidad;
+            }
+
+            set
+            {
+                cantidad = value;
+            }
+        }
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+
+            set
+            {
+                marca = value;
+            }
+        }
+
+        public string Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        public int PrecioListaCordoba
+        {
+            get
+            {
+                return precioListaCordoba;
+            }
+
+            set
+            {
+                precioListaCordoba = value;
+            }
+        }
+
+        public int PrecioListaDollar
+        {
+            get
+            {
+                return precioListaDollar;
+            }
+
+            set
+            {
+                precioListaDollar = value;
+            }
+        }
+    }
+}
