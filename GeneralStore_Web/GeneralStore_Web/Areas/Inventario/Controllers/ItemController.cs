901f51644bd6eced445d93be27faceea4d1231ed
Socram88 <Socram88_programador@Github.com>
2017-09-03 13:04:37 -0600

    Proyecto de Clases

---
+++ GeneralStore_Web/GeneralStore_Web/Areas/Inventario/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore_Web.Areas.Inventario.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Inventario/Item
+        public ActionResult Index()
+        {
+            return View("IndexItem");
+        }
+    }
+}
