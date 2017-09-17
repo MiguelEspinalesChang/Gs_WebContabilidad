37665b3c654e69fa6d04a9fa31521653dbd8280b
Socram88 <Socram88_programador@Github.com>
2017-09-17 10:33:42 -0600

    Graficos Nuevos :D

--- GeneralStore_Web/GeneralStore_Web/scripts/JS/Inventario/MenuInventario.js
+++ GeneralStore_Web/GeneralStore_Web/scripts/JS/Inventario/MenuInventario.js
@@ -1,4 +1,59 @@
 ﻿
+$(document).ready(function () {
+
+    var myChart = Highcharts.chart('grafico', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares January, 2015 to May, 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Microsoft Internet Explorer',
+                y: 56.33
+            }, {
+                name: 'Chrome',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Firefox',
+                y: 10.38
+            }, {
+                name: 'Safari',
+                y: 4.77
+            }, {
+                name: 'Opera',
+                y: 0.91
+            }, {
+                name: 'Proprietary or Undetectable',
+                y: 0.2
+            }]
+        }]
+    });
+
+});
+
+
 var vueObj = new Vue({
     el: "#appVue",
     data: {
@@ -29,5 +84,3 @@ var vueObj = new Vue({
 
 
 });
-
-vueObj.config.debug = true;
